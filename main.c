825c7426459571acd67cfd5e525ea195c6ce9a96
HatecrewJP <138243173+HatecrewJP@users.noreply.github.com>
2026-05-28 14:33:16 +0200

    changed coloring

--- main.c
+++ main.c
@@ -7,6 +7,21 @@
 
 global char *Section = "------------------------------------------------------\n";
 
+#define C_CLEAR "\033[0m"
+
+#define C_OPCODE "\033[38;2;110;71;0m"
+#define C_RD "\033[38;2;204;0;252m"
+#define C_RS1 "\033[38;2;255;0;204m"
+#define C_RS2 "\033[38;2;255;150;147m"
+
+#define C_FUNC3 "\033[38;2;255;255;0m"
+#define C_FUNC7 "\033[38;2;179;255;0m"
+
+#define C_IMM1 "\033[31m"
+#define C_IMM2 "\033[32m"
+
+
+
 internal void SwitchEndian(char *In){
 	for(int i = 0; i < 4; i+=2){
 		char Tmp1,Tmp2;
@@ -45,6 +60,7 @@ internal void HexToBin(char *Src, int SrcLen,char *Dest){
 		}
 	}
 }
+
 
 internal void PrintImmediate(char *Imm){
     printf("%s",Section);
@@ -115,14 +131,30 @@ internal void ProcessIType(char *Instruction){
     memcpy(Imm_11_0,AsBin,12);
     
     printf("InstructionH:0x%s\n",Instruction);
-    printf("InstructionB:0b\033[31m%.12s\033[0m%.20s\n",AsBin, AsBin+12);
+    printf("InstructionB:0b"
+           C_IMM1 "%.12s"
+           C_RS1 "%.5s"
+           C_FUNC3 "%.3s"
+           C_RD "%.5s"
+           C_OPCODE "%.7s"
+           C_CLEAR "\n",Imm_11_0,rs1,func3,rd,Opcode);
     printf("%s",Section);
     
-    printf("Opcode:0b%s\n",Opcode);
-    printf("rd:0b%s\n",rd);
-    printf("func3:0b%s\n",func3);
-    printf("rs1:0b%s\n",rs1);
-    printf("Imm_11_0:0b\033[31m%s\033[0m\n",Imm_11_0);
+    printf("Opcode:"
+           C_OPCODE "0b%s"
+           C_CLEAR"\n",Opcode);
+    printf("rd:"
+           C_RD "0b%s"
+           C_CLEAR "\n",rd);
+    printf("func3:"
+           C_FUNC3 "0b%s"
+           C_CLEAR "\n",func3);
+    printf("rs1:"
+           C_RS1 "0b%s"
+           C_CLEAR "\n",rs1);
+    printf("Imm_11_0:"
+           C_IMM1"0b%s"
+           C_CLEAR "\n",Imm_11_0);
     PrintImmediate(Imm_11_0);
 }
 
@@ -144,19 +176,41 @@ internal void ProcessSType(char *Instruction){
     memcpy(Imm_11_5,AsBin,7);
     
     printf("InstructionH:0x%s\n",Instruction);
-    printf("InstructionB:0b\033[31m%.7s\033[0m%.13s\033[32m%.5s\033[0m%.7s\n",AsBin,AsBin+7,AsBin+20,AsBin+25);
+    printf("InstructionB:0b"
+           C_IMM1"%.7s"
+           C_RS2 "%.5s"
+           C_RS1 "%.5s"
+           C_FUNC3 "%.3s"
+           C_IMM2"%.5s"
+           C_OPCODE "%.7s"
+           C_CLEAR "\n",Imm_11_5, rs2, rs1, func3, Imm_4_0, Opcode);
     printf("%s",Section);
     
-    printf("Opcode:0b%s\n",Opcode);
-    printf("Imm_4_0:0b\033[32m%s\033[0m\n",Imm_4_0);
-    printf("func3:0b%s\n",func3);
-    printf("rs1:0b%s\n",rs1);
-    printf("rs2:0b%s\n",rs2);
-    printf("Imm_11_5:0b\033[31m%s\033[0m\n",Imm_11_5);
+    printf("Opcode:"
+           C_OPCODE" 0b%s"
+           C_CLEAR "\n",Opcode);
+    printf("Imm_4_0:"
+           C_IMM2"0b%s"
+           C_CLEAR "\n",Imm_4_0);
+    printf("func3:"
+           C_FUNC3 "0b%s"
+           C_CLEAR "\n",func3);
+    printf("rs1:"
+           C_RS1 "0b%s"
+           C_CLEAR "\n",rs1);
+    printf("rs2:"
+           C_RS2 "0b%s"
+           C_CLEAR "\n",rs2);
+    printf("Imm_11_5:"
+           C_IMM1"%.7s"
+           C_CLEAR"\n",Imm_11_5);
     
     char Imm[13]={0};
     sprintf(Imm,"%s%s",Imm_11_5,Imm_4_0);
-    printf("Imm: \033[31m%.7s\033[32m%.5s\033[0m\n",Imm,Imm+7);
+    printf("Imm:"
+           C_IMM1"%.7s"
+           C_IMM2"%.5s"
+           C_CLEAR "\n",Imm,Imm+7);
     PrintImmediate(Imm);
 }
 
@@ -298,29 +352,31 @@ int IsValidType(char Type){
     }
 }
 
+
+
 int main(){
-	int IsBigEndian = 1;
+    int IsBigEndian = 1;
     int Running = 1;
-	while(Running){
-		char Type[3] = {0};
-		printf("Type:");
+    while(Running){
+        char Type[3] = {0};
+        printf("Type:");
         if(GetInput(Type,3,"Unknown Type")) continue;
         if(!IsValidType(Type[0])){
             printf("Unknown Type\n");
             continue;
         }
-		if(Type[0]=='Q' || Type[0]=='q'){
-			Running=0;
-			break;
-		}
+        if(Type[0]=='Q' || Type[0]=='q'){
+            Running=0;
+            break;
+        }
         
         
         
-		char Instruction_in[12]={0};
-		char Instruction[9]={0};
+        char Instruction_in[12]={0};
+        char Instruction[9]={0};
         
         if(Type[0]!='E' && Type[0]!='e'){
-			printf("32-Bit Instruction in Hex:");
+            printf("32-Bit Instruction in Hex:");
             if(GetInput(Instruction_in,12,"Instruction is not 32bit")) continue;
             if(!strncmp(Instruction_in,"0x",2)){
                 memcpy(Instruction_in,Instruction_in+2,10);
@@ -349,42 +405,42 @@ int main(){
                 SwitchEndian(Instruction);
             }
             
-		}
+        }
         
         printf("%s",Section);
         printf("%s",Section);
-		switch(Type[0]){
-			case 'R':
-			case 'r':{
-				ProcessRType(Instruction);
-				
-			}break;
-			case 'I':
-			case 'i':{
+        switch(Type[0]){
+            case 'R':
+            case 'r':{
+                ProcessRType(Instruction);
+                
+            }break;
+            case 'I':
+            case 'i':{
                 ProcessIType(Instruction);
-			}break;
-			
-			case 'S':
-			case 's':{
-                ProcessSType(Instruction);
-			}break;
-			
-			case 'B':
-			case 'b':{
-                ProcessBType(Instruction);
-			}break;
-			
-			case 'U':
-			case 'u':{
-                ProcessUType(Instruction);
-			}break;
-			
-			case 'J':
-			case 'j':{
-                ProcessJType(Instruction);
-			}break;
+            }break;
             
-			case 'E':
+            case 'S':
+            case 's':{
+                ProcessSType(Instruction);
+            }break;
+            
+            case 'B':
+            case 'b':{
+                ProcessBType(Instruction);
+            }break;
+            
+            case 'U':
+            case 'u':{
+                ProcessUType(Instruction);
+            }break;
+            
+            case 'J':
+            case 'j':{
+                ProcessJType(Instruction);
+            }break;
+            
+            case 'E':
             case 'e':{
                 char Endian[3] = {0};
                 printf("EndianFormat:\n-1:Big Endian\n-0:Little Endian\n");
@@ -395,16 +451,16 @@ int main(){
                 if(GetInput(Endian,3,"Unknown Endian Format")) continue;
                 IsBigEndian=Endian[0]-'0';
             }continue;
-			case 'Q':
-			case 'q':{
-				Assert(0);
-			}break;
+            case 'Q':
+            case 'q':{
+                Assert(0);
+            }break;
             
-			
-		}
+            
+        }
         printf("%s",Section);
         printf("%s",Section);
         
         
-	}
+    }
 }
